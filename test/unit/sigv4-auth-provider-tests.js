1e7ffe168416727e04b4027715059a9e831618c4
jbarney <jbarney@xlfleet.com>
2020-12-17 11:37:58 -0500

    unify manual and credential chain credentials
    removed a credential resolution function that always returned undefined
    fetch (potentially cached) credentials when needed--at the challange

--- test/unit/sigv4-auth-provider-tests.js
+++ test/unit/sigv4-auth-provider-tests.js
@@ -16,6 +16,7 @@
 
 'use strict';
 
+const AWS = require('aws-sdk');
 const lib = require('../../lib/sigv4-auth-provider.js');
 const assert = require('assert');
 
@@ -66,7 +67,7 @@ describe('SigV4AuthProvider',  () => {
       assert.equal(provider.region, "us-east-23");
     });
 
-    it('should use default if Provided',  () => {
+    it('should use default region if Provided',  () => {
       let provider = new SigV4AuthProvider({accessKeyId:'key'});
 
       assert.equal(provider.region, "ENV_DEFAULT_REGION");
@@ -79,6 +80,37 @@ describe('SigV4AuthProvider',  () => {
           "[SIGV4_MISSING_REGION] No region provided.  You must either provide a region or set "
           + "environment variable [AWS_REGION]");
       assert.throws(() => {new SigV4AuthProvider()}, err);
+    });
+
+    it('should use provided credentials', (done) => {
+      const credentials = new AWS.Credentials('UserID-1', 'UserSecretKey-1', 'SessiosnToken-1');
+      const options = {
+        region: "us-east-23",
+        accessKeyId: "UserID-1",
+        secretAccessKey: "UserSecretKey-1",
+        sessionToken: "SessiosnToken-1"
+      };
+      let provider = new SigV4AuthProvider(options);
+      assert.ok(provider.chain);
+      assert.ok(provider.chain instanceof AWS.CredentialProviderChain);
+      assert.deepEqual(provider.chain.providers, [credentials]);
+      provider.chain.resolvePromise().then((creds) => {
+        assert.deepEqual(creds, {
+          accessKeyId: 'UserID-1',
+          expireTime: null,
+          expired: false,
+          refreshCallbacks: [],
+          sessionToken: 'SessiosnToken-1'
+        });
+        done();
+      });
+    });
+
+    it('should use default credential chain if no access key provided', () => {
+      let provider = new SigV4AuthProvider({region: "us-east-23"});
+      assert.ok(provider.chain);
+      assert.ok(provider.chain instanceof AWS.CredentialProviderChain);
+      assert.deepEqual(provider.chain.providers, AWS.CredentialProviderChain.defaultProviders);
     });
   });
 });
@@ -96,30 +128,29 @@ describe('SigV4Authenticator', () => {
         // this is a style of buffer setup that is deprecated, however
         // it is consistent with older versions of js.  We use it
         // here as a double-entry bookkeeping that our buffer is right.
-        assert.notStrictEqual(buf, new Buffer("SigV4\0\0", 'utf8'));
-      })
+        assert.notStrictEqual(buf, new Buffer.from("SigV4\0\0", 'utf8'));
+      });
     });
   });
 
   describe('#evaluateChallenge()', () => {
+    const credentials = new AWS.Credentials('UserID-1', 'UserSecretKey-1', 'SessiosnToken-1');
+    const chain = new AWS.CredentialProviderChain([credentials]);
     let target = new lib.SigV4Authenticator({
       region: 'us-west-2',
-      accessKeyId: 'UserID-1',
-      secretAccessKey: 'UserSecretKey-1',
-      sessionToken: 'SessiosnToken-1',
+      chain: chain,
       date: new Date(1591742511000)
     });
 
-    it('should call callback with Signed Request', () => {
+    it('should call callback with Signed Request', (done) => {
       let nonceBuffer = Buffer.from("nonce=91703fdc2ef562e19fbdab0f58e42fe5");
       let expected = "signature=7f3691c18a81b8ce7457699effbfae5b09b4e0714ab38c1292dbdf082c9ddd87,access_key=UserID-1,amzdate=2020-06-09T22:41:51.000Z,session_token=SessiosnToken-1";
 
       let calledCallback = false;
       target.evaluateChallenge(nonceBuffer, (err, buff) => {
         assert.equal(buff.toString(), expected);
-        calledCallback = true;
+        done();
       });
-      assert.equal(calledCallback, true);
     });
 
     it('should fail when Nonce is not found', () => {
